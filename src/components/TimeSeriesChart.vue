abff9552365c5750cd187ceee2f1cf5cc1c2bdda
Logan <1009728013@qq.com>
2025-02-20 02:12:02 +0800

    feat: resize

--- src/components/TimeSeriesChart.vue
+++ src/components/TimeSeriesChart.vue
@@ -6,65 +6,16 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, onUnmounted, ref } from 'vue'
 import * as echarts from 'echarts'
+import { data, markLineData, markAreaData } from './data'
 
-
-const data = [
-  [0, 0.0288930497090785],
-  [1, 0.4030400109335138],
-  [2, -0.3665846621297786],
-  [3, -0.3697103413119156],
-  [4, -0.3864548379991166],
-  [5, 0.4030400109335138],
-  [6, -0.381953177045768],
-  [7, -0.3526316611922266],
-  [8, -0.3656054056384419],
-  [9, -0.3590148904354906],
-  [10, 0.4030400109335138],
-  [11, -0.3867574783841583],
-  [12, -0.3548944861112799],
-  [13, -0.2415738484925692],
-  [14, -0.3537564570537629],
-  [15, 0.4030400109335138],
-  [16, -0.3571556982658774],
-  [17, -0.3467438689708244],
-  [18, 0.4030400109335138],
-  [19, -0.3557156400591761],
-]
-
-const markLineData = [
-  {
-    xAxis: '5',
-  },
-  {
-    xAxis: '6'
-  },
-]
-
-const markAreaData = [
-  [
-    {
-      xAxis: '10',
-    },
-    {
-      xAxis: '11'
-    }
-  ],
-  [
-    {
-      xAxis: '15',
-    },
-    {
-      xAxis: '16'
-    }
-  ],
-]
 
 const chartContainer = ref(null)
+let myChart = null
 
 onMounted(() => {
-  const myChart = echarts.init(chartContainer.value)
+  myChart = echarts.init(chartContainer.value)
   const option = {
     series: [
       {
@@ -99,7 +50,8 @@ onMounted(() => {
       {
         type: 'slider',
         xAxisIndex: 0,
-        filterMode: 'none'
+        filterMode: 'none',
+        endValue: 100
       },
       {
         type: 'slider',
@@ -111,16 +63,27 @@ onMounted(() => {
         xAxisIndex: 0,
         filterMode: 'none'
       },
-      {
-        type: 'inside',
-        yAxisIndex: 0,
-        filterMode: 'none'
-      }
     ],
     tooltip: {
     },
   }
   myChart.setOption(option)
+
+  const resizeObserver = new ResizeObserver(() => {
+    if (myChart) {
+      myChart.resize()
+    }
+  })
+
+  resizeObserver.observe(chartContainer.value)
+
+  onUnmounted(() => {
+    resizeObserver.disconnect()
+    if (myChart) {
+      myChart.dispose()
+      myChart = null
+    }
+  })
 })
 </script>
 
