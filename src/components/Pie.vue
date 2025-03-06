6f47443694b2cb33a5a8757bb936cecfcdb0dabf
Logan <1009728013@qq.com>
2025-03-07 05:47:27 +0800

    feat: Pie & Radar

--- src/components/Pie.vue
+++ src/components/Pie.vue
@@ -1,8 +1,63 @@
 <script setup>
+import { onMounted, ref } from 'vue'
+import * as echarts from 'echarts'
+
+const chartContainer = ref(null)
+
+onMounted(() => {
+  const myChart = echarts.init(chartContainer.value)
+  const option = {
+    tooltip: {
+      trigger: 'item'
+    },
+    legend: {
+      top: '5%',
+      left: 'center'
+    },
+    series: [
+      {
+        name: '异常类型',
+        type: 'pie',
+        radius: ['40%', '70%'],
+        avoidLabelOverlap: false,
+        padAngle: 5,
+        itemStyle: {
+          borderRadius: 10
+        },
+        label: {
+          show: false,
+          position: 'center'
+        },
+        emphasis: {
+          label: {
+            show: true,
+            fontSize: 40,
+            fontWeight: 'bold'
+          }
+        },
+        labelLine: {
+          show: false
+        },
+        data: [
+          { value: 104, name: 'A' },
+          { value: 735, name: 'B' },
+          { value: 580, name: 'C' },
+          { value: 484, name: 'D' },
+          { value: 500, name: 'E' }
+        ]
+      }
+    ]
+  }
+
+  myChart.setOption(option)
+})
 </script>
 
 <template>
-  <h2 class="text-sky-500">Pie</h2>
+  <div
+    class="h-[80vh]"
+    ref="chartContainer"
+  ></div>
 </template>
 
 <style scoped></style>
--- src/components/Radar.vue
+++ src/components/Radar.vue
@@ -1,8 +1,56 @@
 <script setup>
+import { onMounted, ref } from 'vue'
+import * as echarts from 'echarts'
+
+const chartContainer = ref(null)
+
+onMounted(() => {
+  const myChart = echarts.init(chartContainer.value)
+  const option = {
+    title: {
+      text: '性能指标'
+    },
+    legend: {
+      data: ['ARIMA', 'TadGAN']
+    },
+    radar: {
+      // shape: 'circle',
+      indicator: [
+        { name: 'PA-F1', max: 1 },
+        { name: 'Event-based-F1	', max: 1 },
+        { name: 'R-based-F1', max: 1 },
+        { name: 'Affiliation-F1', max: 1 },
+        { name: 'AUC-PR', max: 1 },
+        { name: 'AUC-ROC', max: 1 }
+      ]
+    },
+    series: [
+      {
+        name: 'ARIMA | TadGAN',
+        type: 'radar',
+        data: [
+          {
+            value: [0.42, 0.30, 0.20, 0.35, 0.50, 0.18],
+            name: 'ARIMA'
+          },
+          {
+            value: [0.50, 0.14, 0.28, 0.26, 0.42, 0.21],
+            name: 'TadGAN'
+          }
+        ]
+      }
+    ]
+  }
+
+  myChart.setOption(option)
+})
 </script>
 
 <template>
-  <h2 class="text-sky-500">Radar</h2>
+  <div
+    class="h-[80vh]"
+    ref="chartContainer"
+  ></div>
 </template>
 
 <style scoped></style>
