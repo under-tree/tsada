1dca575c08625914f0f0422a8d5e69b056d6b35e
Logan <1009728013@qq.com>
2025-05-02 02:36:57 +0800

    feat: responsive layout

--- src/pages/DemoF.vue
+++ src/pages/DemoF.vue
@@ -1,9 +1,60 @@
 <script setup>
-import AnomalyViewer from '../components/AnomalyViewer.vue'
+import { ref } from 'vue'
 </script>
 
 <template>
-  <AnomalyViewer />
+  <div class="max-w-250 m-0 m-auto">
+
+    <el-row :gutter="16">
+
+      <el-col
+        :span="16"
+        :xs="24"
+        class="mb-4"
+      >
+        <el-card class="h-80">
+          <div class="h-80">
+          </div>
+        </el-card>
+      </el-col>
+
+      <el-col
+        :span="8"
+        :xs="24"
+      >
+
+        <el-row :gutter="16">
+          <el-col
+            :span="24"
+            :xs="12"
+            class="mb-4"
+          >
+            <el-card class="h-37.5">
+            </el-card>
+          </el-col>
+
+          <el-col
+            :span="24"
+            :xs="12"
+          >
+            <el-card class="h-37.5 ">
+            </el-card>
+          </el-col>
+        </el-row>
+
+      </el-col>
+
+    </el-row>
+
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.el-row {
+  margin-bottom: 1rem;
+}
+
+.el-col {
+  border-radius: 1rem;
+}
+</style>
